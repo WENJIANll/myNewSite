fa2c6e848eb290663b0296788d6dbf1e4840e656
WUYNE <1305133324@qq.com>
2020-10-18 14:30:37 +0800

    master

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # 设置了别名
+    # 127.0.0.1:8000/blog/
+    # start with blog
+    path('',views.blog_list, name="blog_list"),
+    path('<int:blog_pk>',views.blog_detail, name="blog_detail"),
+    path('type/<int:blog_type_pk>',views.blogs_with_type,name="blogs_with_type")
+    # # re_path('^$',views.index),
+]
