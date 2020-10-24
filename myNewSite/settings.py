f6613b878d6b74920af681709269d4451c5b3f40
WUYNE <1305133324@qq.com>
2020-10-24 22:27:54 +0800

    master

--- myNewSite/settings.py
+++ myNewSite/settings.py
@@ -177,6 +177,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = '1305133324@qq.com'
-EMAIL_HOST_PASSWORD = 'faxkpppobcdogeie'  # 授权码
+EMAIL_HOST_PASSWORD = ''  # 授权码
 EMAIL_SUBJECT_PREFIX = '[##兴趣使然##]'
 EMAIL_USE_TLS = True  # 与SMTP服务器通信时，是否启动TLS链接(安全链接)
